5a0d1b588826167cd8ebdf75ee6d08c797d52015
Hany-Kim <hanykim1210@gmail.com>
2024-11-07 12:52:45 +0900

    pjt init commit

---
+++ blackWhiteDeliveryDriver/src/main/java/com/sparta/blackwhitedeliverydriver/BlackWhiteDeliveryDriverApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.blackwhitedeliverydriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackWhiteDeliveryDriverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackWhiteDeliveryDriverApplication.class, args);
+    }
+
+}
---
+++ blackWhiteDeliveryDriver/src/test/java/com/sparta/blackwhitedeliverydriver/BlackWhiteDeliveryDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.blackwhitedeliverydriver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackWhiteDeliveryDriverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
