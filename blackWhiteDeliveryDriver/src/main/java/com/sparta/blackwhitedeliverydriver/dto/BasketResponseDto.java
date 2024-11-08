4a55a3f4643df0b74b80b1a482ff781105538cda
strongcookdas <goodjav141592@gmail.com>
2024-11-08 17:44:10 +0900

    feat(basket) : 장바구니 담기 api 구현 - 장바구니 담기 controller 테스트 추가 - 장바구니 담기 api controller layer 구현

---
+++ blackWhiteDeliveryDriver/src/main/java/com/sparta/blackwhitedeliverydriver/dto/BasketResponseDto.java
@@ -0,0 +1,14 @@
+package com.sparta.blackwhitedeliverydriver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class BasketResponseDto {
+    private String basketId;
+}
