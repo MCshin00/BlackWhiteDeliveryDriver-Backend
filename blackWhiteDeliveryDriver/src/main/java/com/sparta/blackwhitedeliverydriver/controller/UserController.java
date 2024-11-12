302eabca8e2eb37fc8189889c7692e0e091b3e36
MCshin00 <sinminc7@gmail.com>
2024-11-12 14:20:15 +0900

    refactor: base uri 변경

--- blackWhiteDeliveryDriver/src/main/java/com/sparta/blackwhitedeliverydriver/controller/UserController.java
+++ blackWhiteDeliveryDriver/src/main/java/com/sparta/blackwhitedeliverydriver/controller/UserController.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users")
+@RequestMapping("/api/v1/users")
 public class UserController {
 
     private final UserService userService;
