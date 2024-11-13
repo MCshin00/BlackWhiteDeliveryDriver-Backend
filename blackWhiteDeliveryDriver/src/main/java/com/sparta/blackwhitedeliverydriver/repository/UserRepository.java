4c6f851c1f077f0e43561f2285d1bbbd0361b32c
strongcookdas <goodjav141592@gmail.com>
2024-11-13 10:11:57 +0900

    chore : UserRepository, application.properties 원상복구

--- blackWhiteDeliveryDriver/src/main/java/com/sparta/blackwhitedeliverydriver/repository/UserRepository.java
+++ blackWhiteDeliveryDriver/src/main/java/com/sparta/blackwhitedeliverydriver/repository/UserRepository.java
@@ -4,7 +4,7 @@ import com.sparta.blackwhitedeliverydriver.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     Optional<User> findByEmail(String email);
