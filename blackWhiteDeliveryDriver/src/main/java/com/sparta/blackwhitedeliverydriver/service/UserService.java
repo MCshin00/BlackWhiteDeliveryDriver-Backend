fc3556d4c637f41a53ee48eda0b63eb703eafc11
MCshin00 <sinminc7@gmail.com>
2024-11-14 15:11:20 +0900

    refactor: Service에서 로그인된 사용자 정보를 가져오는 로직 변경
    기존 코드는 SecurityContextHoler에서 가져와 의존성 문제와 테스트가 어려운 문제가 발생
    따라서 AuditorAware로 정보를 가져오도록 변경

--- blackWhiteDeliveryDriver/src/main/java/com/sparta/blackwhitedeliverydriver/service/UserService.java
+++ blackWhiteDeliveryDriver/src/main/java/com/sparta/blackwhitedeliverydriver/service/UserService.java
@@ -13,9 +13,6 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -39,7 +36,8 @@ public class UserService {
         User user = User.from(requestDto, passwordEncoder);
 
         // 로그인된 사용자가 있을 경우 그 사용자의 username을 CreatedBy로 설정, 없는 경우 회원가입 시 지정한 username이 됨
-        String createdBy = getCurrentUsername(requestDto.getUsername());
+        String createdBy = auditorAware.getCurrentAuditor()
+                .orElse(requestDto.getUsername());
         user.setCreatedBy(createdBy);
         user.setLastModifiedBy(createdBy);
         User savedUser = userRepository.save(user);  // User 엔티티 저장
@@ -118,17 +116,6 @@ public class UserService {
         if (role != UserRoleEnum.CUSTOMER && role != UserRoleEnum.OWNER) {
             throw new IllegalArgumentException(ExceptionMessage.NOT_ALLOEWD_ROLE.getMessage());
         }
-    }
-
-    // 현재 로그인된 사용자의 username을 가져오는 메서드
-    private String getCurrentUsername(String username) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        // 인증되지 않은 경우, 즉 anonymousUser일 경우
-        if (authentication instanceof AnonymousAuthenticationToken) {
-            return username;  // 로그인되지 않은 경우, 요청된 username을 return
-        }
-        // 로그인된 경우, 사용자 이름을 반환
-        return authentication.getName();
     }
 
     private void checkDeletedUser(User user) {
