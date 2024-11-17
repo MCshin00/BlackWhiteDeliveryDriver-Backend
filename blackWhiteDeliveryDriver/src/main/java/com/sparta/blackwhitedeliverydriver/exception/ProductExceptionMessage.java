9242178388f44e946d228d690097efc0bc35c96c
Hany-Kim <hanykim1210@gmail.com>
2024-11-18 02:11:57 +0900

    refactor : Exception Message Enum으로 관리

--- blackWhiteDeliveryDriver/src/main/java/com/sparta/blackwhitedeliverydriver/exception/ProductExceptionMessage.java
+++ blackWhiteDeliveryDriver/src/main/java/com/sparta/blackwhitedeliverydriver/exception/ProductExceptionMessage.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ProductExceptionMessage {
-    PRODUCT_NOT_FOUND("존재하지 않는 음식 정보입니다.");
+    PRODUCT_NOT_FOUND("존재하지 않는 음식 정보입니다."),
+    DUPLICATED_STORE_NAME("중복된 음식입니다.");
     private final String message;
 
 
