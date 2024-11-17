1af087a900f2e01c45e7e152cedacc5c9011057f
MCshin00 <sinminc7@gmail.com>
2024-11-17 10:41:02 +0900

    feat: 카테고리 목록 조회 기능 구현

---
+++ blackWhiteDeliveryDriver/src/main/java/com/sparta/blackwhitedeliverydriver/controller/CategoryController.java
@@ -0,0 +1,39 @@
+package com.sparta.blackwhitedeliverydriver.controller;
+
+import com.sparta.blackwhitedeliverydriver.dto.CategoryIdResponseDto;
+import com.sparta.blackwhitedeliverydriver.dto.CategoryRequestDto;
+import com.sparta.blackwhitedeliverydriver.dto.CategoryResponseDto;
+import com.sparta.blackwhitedeliverydriver.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/category")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping("/")
+    public ResponseEntity<Page<CategoryResponseDto>> getAllCategories(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc) {
+
+        Page<CategoryResponseDto> categoryResponseDtos = categoryService.getAllCategories(page-1, size, sortBy, isAsc);
+
+        return ResponseEntity.status(HttpStatus.OK).body(categoryResponseDtos);
+    }
+}
--- blackWhiteDeliveryDriver/src/main/java/com/sparta/blackwhitedeliverydriver/service/CategoryService.java
+++ blackWhiteDeliveryDriver/src/main/java/com/sparta/blackwhitedeliverydriver/service/CategoryService.java
@@ -1,9 +1,17 @@
 package com.sparta.blackwhitedeliverydriver.service;
 
+import com.sparta.blackwhitedeliverydriver.dto.CategoryIdResponseDto;
+import com.sparta.blackwhitedeliverydriver.dto.CategoryRequestDto;
+import com.sparta.blackwhitedeliverydriver.dto.CategoryResponseDto;
 import com.sparta.blackwhitedeliverydriver.entity.Category;
 import com.sparta.blackwhitedeliverydriver.entity.User;
+import com.sparta.blackwhitedeliverydriver.exception.CategoryExceptionMessage;
 import com.sparta.blackwhitedeliverydriver.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,8 +21,8 @@ import java.util.*;
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
-    private final CategoryRepository categoryRepository;
 
+    private final CategoryRepository categoryRepository;
 
     @Transactional
     public List<Category> getOrCreateCategory(String categoryNames, User user) {
@@ -36,5 +44,20 @@ public class CategoryService {
         }
 
         return categoryList;
+    }
+
+    public Page<CategoryResponseDto> getAllCategories(int page, int size, String sortBy, boolean isAsc) {
+        if (size != 10 && size != 30 && size != 50) {
+            size = 10;
+        }
+
+        // 페이징 처리
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        Page<Category> categoryPage = categoryRepository.findAll(pageable);
+
+        return categoryPage.map(CategoryResponseDto::from);
     }
 }
